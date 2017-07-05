6ddd6634bd6a859dc0c22e3451623af0aa61bb97
nfjones87 <nfjones87@gmail.com>
2017-07-05 12:27:07 -0700

    working single toggle

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ $(function() {
         $('.mainmenu').toggle()
     });
 
-     $('.item').click((e) => {
-        $(e.target).find('.dropdown').toggle()
-    });
 
     $('a').click((e) => {
-        $(e.target).closest('.item').find('.dropdown').toggle()
+        $(e.target).siblings('.dropdown').toggle()
     });
 
 
